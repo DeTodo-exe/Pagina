a0ae341f5946d14446a5faa14c4815264178410d
ManuelM11 <86327401+ManuelM11@users.noreply.github.com>
2022-07-23 18:52:11 -0400

    Update productmodel.js

--- Paginas/productmodel.js
+++ Paginas/productmodel.js
@@ -1,6 +1,19 @@
 export class ProductModel{
     
-    constructor(id,){
-    
+    constructor(id,name,price,discount,category,stock,description){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+        this.category = category;
+        this.stock = stock;
+        this.description = description;
     }
+    getId(){return this.id}
+    getName(){return this.name}
+    getPrice(){return this.price}
+    getDiscount(){return this.discount}
+    getCategory(){return this.category}
+    getStock(){return this.stock}
+    getDescription(){return this.description}
 }
